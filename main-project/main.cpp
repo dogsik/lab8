2c7884e2a2a7477dc1237e692752c3674596a706
dogsik <83946345+dogsik@users.noreply.github.com>
2022-01-17 15:31:32 +0300

    Добавлен вывод общей информации о задании

--- main-project/main.cpp
+++ main-project/main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main()
 {
     cout << "Laboratory work #8. GIT\n";
-    cout << "Variant #5. Internet Protocol\n";
+    cout << "Variant #5. Internet Protocol.\n";
     cout << "Author: Kozlova Anastasia\n";
     return 0;
 }
