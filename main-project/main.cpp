bbb6903bfff6f7120306385b73a3a07598639021
dogsik <meow_dogs@mail.ru>
2022-01-17 18:02:55 +0300

    Создана заготовка проекта

---
+++ main-project/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Laboratory work #8. GIT\n";
+    cout << "Variant #5. Internet Protocol\n";
+    cout << "Author: Kozlova Anastasia\n";
+    return 0;
+}
