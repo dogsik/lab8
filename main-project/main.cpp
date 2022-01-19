c1cad5383cf820aac5a53ff5136c52c5a947b1a3
dogsik <83946345+dogsik@users.noreply.github.com>
2022-01-19 13:49:09 +0300

    Добавлены тестовые данные

--- main-project/main.cpp
+++ main-project/main.cpp
@@ -5,7 +5,8 @@ using namespace std;
 int main()
 {
     cout << "Laboratory work #8. GIT\n";
-    cout << "Variant #5. Internet Protocol\n";
+    cout << "Variant #5. Internet Protocol.\n";
     cout << "Author: Kozlova Anastasia\n";
+    cout << "Group: 14\n";
     return 0;
 }
